8e401a25b0df3c45456fea68b8f3a5dfbd9dac64
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-08-12 19:54:50 +0100

    Add is_tabular tests

---
+++ tests/testthat/test-is_tabular.R
@@ -0,0 +1,18 @@
+test_that("is_tabular returns the correct value", {
+  expect_true(is_tabular(mtcars))
+  expect_true(is_tabular(iris))
+
+  expect_true(is_tabular(as.environment(mtcars)))
+  expect_true(is_tabular(as.environment(iris)))
+
+  uneven_list <- list(
+    numeric = c(1, 2, 3),
+    character = letters
+  )
+
+  expect_false(is_tabular(uneven_list))
+
+  uneven_env <- as.environment(uneven_list)
+
+  expect_false(is_tabular(uneven_env))
+})
