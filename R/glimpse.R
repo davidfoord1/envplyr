2f84d96643619e155bf7d557349f54733fb23ddf
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-08-14 21:08:55 +0100

    Reorganise glimpse
    Add special case for environment self containment

--- R/glimpse.R
+++ R/glimpse.R
@@ -19,6 +19,9 @@ glimpse.environment <- function(x, width = NULL, ...) {
     first_obj <- x[[names(x)[[1]]]]
     cli::cat_line("Rows: ", length(first_obj))
     cli::cat_line("Columns: ", length(x))
+  } else {
+    cli::cat_line("Objects: ", length(x))
+  }
 
     obj_types <- lapply(eapply(x, pillar::new_pillar_type, all.names = TRUE), format)
     obj_names <- format(pillar::new_pillar_title(names(x)))
@@ -28,8 +31,12 @@ glimpse.environment <- function(x, width = NULL, ...) {
       x,
       \(obj) {
         contents <- format(obj)
+
         if (!is.null(names(obj))) {
           contents <- names(obj)
+          if (identical(obj, x)) {
+            contents <- cli::col_br_blue(format(obj))
+          }
         }
 
         paste(contents, collapse = ", ")
@@ -37,15 +44,16 @@ glimpse.environment <- function(x, width = NULL, ...) {
       all.names = TRUE
     )
 
+    contents_width <- width - nchar(obj_names)
+
+    ellipsis <- ifelse(nchar(obj_contents) > contents_width, "...", "")
+
     truncated <- paste0(
-      substr(obj_contents, 1, width - nchar(obj_names) - 3),
-      "..."
+      substr(obj_contents, 1, contents_width - nchar(ellipsis)),
+      ellipsis
     )
 
-    cli::cat_line(obj_names, " ", truncated)
-  } else {
-    cli::cat_line("Objects: ", length(x))
-  }
+  cli::cat_line(obj_names, " ", truncated)
 
   cli::cat_line(cli::col_br_blue(format(x)))
   invisible(x)
