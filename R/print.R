45db53a366f2b91d8b844ac881550393bfc22083
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-08-15 13:24:36 +0100

    Exclude hidden objects from print

--- R/print.R
+++ R/print.R
@@ -9,12 +9,16 @@
 print.environment <- function(x, ...) {
   cli::cat_line(cli::col_br_blue(format(x)))
 
+
+  # exclude hidden objects
+  obj_names <- names(x)[!startsWith(names(x), ".")]
+
   if (is_tabular(x)) {
     num_rows <- length(x[[names(x)[[1]]]])
-    num_cols <- length(x)
+    num_cols <- length(obj_names)
     cli::cat_line(cli::col_grey("# A tabular environment: ", num_rows, " x ", num_cols))
   } else {
-    cli::cat_line(cli::col_grey("# An environment with ", length(x), " objects"))
+    cli::cat_line(cli::col_grey("# An environment with ", length(obj_names), " objects"))
   }
 
   invisible(x)
