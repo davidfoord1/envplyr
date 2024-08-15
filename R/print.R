07ca9e9e1f6c1a42a183b0d9e2c487cfc06533bc
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-08-15 10:55:03 +0100

    Update print.R

--- R/print.R
+++ R/print.R
@@ -10,12 +10,12 @@ print.environment <- function(x, ...) {
   cli::cat_line(cli::col_br_blue(format(x)))
 
   if (is_tabular(x)) {
-    dimensions <- paste(length(x[[names(x)[[1]]]]), "x", length(x))
+    num_rows <- length(x[[names(x)[[1]]]])
+    num_cols <- length(x)
+    cli::cat_line(cli::col_grey("# A tabular environment: ", num_rows, " x ", num_cols))
+  } else {
+    cli::cat_line(cli::col_grey("# An environment with ", length(x), " objects"))
   }
-
-  cli::cat_line(cli::col_grey("# An environment: ", dimensions))
-
-  pillar::pillar()
 
   invisible(x)
 }
